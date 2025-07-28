178c87b7027db0ad506f8bd73387c2cb73e082c5
George <georgemwazuna3@gmail.com>
2025-07-28 10:06:11 +0300

    made  a  few small changes to enable the page to work

--- bus-booking-frontend/src/components/Navbar.jsx
+++ bus-booking-frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext.jsx";
 import "./NavBar.css"; // Assuming you have a CSS file for styling
 
 const Navbar = () => {
